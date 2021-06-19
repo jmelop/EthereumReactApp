c228b57d756d6d6164c6f1986846db44ddd2463b
Juan Melo Pérez <juanmlstm@gmail.com>
2021-06-19 16:51:53 +0100

    Fixed few bugs

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -16,10 +16,42 @@ contract Token {
         owner = msg.sender;
     }
 
+    function generateRandomNumber() public view returns (uint256) {
+        uint256 randomNumber =
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        block.timestamp +
+                            block.difficulty +
+                            ((
+                                uint256(
+                                    keccak256(abi.encodePacked(block.gaslimit))
+                                )
+                            ) / (block.timestamp)) +
+                            block.gaslimit +
+                            ((
+                                uint256(keccak256(abi.encodePacked(msg.sender)))
+                            ) / (block.timestamp)) +
+                            block.number
+                    )
+                )
+            );
+
+        return (randomNumber - ((randomNumber / 1000) * 1000));
+    }
+
     function transfer(address to, uint256 amount) external {
         require(balances[msg.sender] >= amount, "Not enough tokens");
         balances[msg.sender] -= amount;
         balances[to] += amount;
+    }
+
+    function calculateMathProblem(address to, uint256 solution) external {
+        uint256 randomNumber = generateRandomNumber();
+        require(solution == randomNumber, "Solucion incorrecta!");
+        balances[msg.sender] -= 20;
+        balances[to] += 20;
+        console.log("nice");
     }
 
     function balanceOf(address account) external view returns (uint256) {
--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { useState } from 'react';
+import { Component, useState } from 'react';
 import { ethers, providers } from 'ethers';
 import './App.css';
 import Token from './artifacts/contracts/Token.sol/Token.json';
 import './index.css';
 import logo from '../src/assets/giphy.gif'
+import Web3 from 'web3'
 
 
-const tokenAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+const tokenAddress = '0x0165878A594ca255338adfa4d48449f69242Eb8F';
 
 function App() {
 
@@ -15,12 +16,13 @@ function App() {
   var wallet = '';
   getTypeWallet();
 
+
   async function requestAccount() {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
   }
 
   function getTypeWallet() {
-    if(typeof window.web3 === 'undefined'){
+    if (typeof window.web3 === 'undefined') {
       console.log('Error, please install MetaMask, Trust or Coinbase wallet');
     } else if (window.web3.currentProvider.isMetaMask) {
       wallet = 'MetaMask';
@@ -31,24 +33,68 @@ function App() {
     } else if (window.web3.currentProvider.isToshi) {
       wallet = 'CoinBase';
       console.log('CoinBase');
-    } 
+    }
+  }
+
+  async function getWalletInfo() {
+    if (wallet) {
+      try {
+        const account = await window.ethereum.request({ method: 'eth_accounts' });
+        if (account.length === 0) {
+          console.log('Error, not able to get accounts')
+        } else {
+          console.log(account);
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
   }
 
 
   async function getBalance() {
     if (wallet) {
+      const web3 = new Web3(window.ethereum);
       const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
       const balance = await contract.balanceOf(account);
-      console.log("Balance: ", balance.toString());
+
+      const ethBalance = web3.eth.getBalance(account).then(balance => {
+        let balanceFormatted = balance.toString();
+        balanceFormatted = balanceFormatted.slice(0, -12);
+        console.log('Balance ETH: ' + balanceFormatted.toString());
+        alert('Balance ETH: ' + balanceFormatted.toString());
+      });
+
+      console.log("Balance: JMT" + balance.toString());
+      alert("Balance JMT: " + balance.toString());
     }
   }
 
 
+   async function initializeMathProblem(){
+    if (wallet) {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
+      const transaction = await contract.calculateMathProblem(userAccount, 4);
+      await transaction.wait().then(u => {
+        if(typeof u !== 'undefined'){
+        console.log(u);
+        } else {
+          console.log('Error, la solución no es correcta')
+        }
+      }).catch((err) => (console.log(err)))
+      console.log(`Coins successfully sent to ${userAccount}`);
+
+    }
+  } 
+
   async function sendCoins() {
     if (wallet) {
-      await requestAccount()
+      await requestAccount();
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
@@ -58,19 +104,20 @@ function App() {
     }
   }
 
-
   return (
     <div className="App">
-      <header className="App-header">          
+      <header className="App-header">
         <img src={logo} alt="loading..." class="w-40 mb-8" />
         <p>Type of wallet: {wallet}</p>
         <div class="flex">
           <div class="p-20 flex flex-col container bg-gray-100 items-center m-2 rounded-lg">
-            <input class="rounded m-1 text-gray-700" onChange={e => setUserAccount(e.target.value)} placeholder="Account ID" />
+            <input class="rounded m-1 text-gray-700" onChange={e => setUserAccount(e.target.value)} placeholder="Account ID"/>
             <input type="number" class="rounded text-gray-700	"
               onChange={e => setAmount(e.target.value)} placeholder="Amount" />
             <button class="bg-white text-gray-700 m-2 rounded-lg p-1.5 mt-8" onClick={getBalance}>Get Balance</button>
             <button class="bg-white text-gray-700 m-2 rounded-lg p-1.5" onClick={sendCoins}>Send Coins</button>
+            <button class="bg-white text-gray-700 m-2 rounded-lg p-1.5" onClick={getWalletInfo}>Get Wallet Info</button>
+            {<button class="bg-white text-gray-700 m-2 rounded-lg p-1.5" onClick={initializeMathProblem}>Math Problem</button>} 
           </div>
         </div>
 
@@ -80,5 +127,8 @@ function App() {
     </div>
   );
 }
+
+
+
 
 export default App;
